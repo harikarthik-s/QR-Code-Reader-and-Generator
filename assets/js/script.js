edf67d1a2353fdcef5386e9ba1dcd397a5b0240f
harikarthik-s <harikarthiks15@gmail.com>
2024-03-17 23:18:08 +0530

    QR scanner completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,9 @@ document.querySelector(".generate button").addEventListener("click", (e) => {
   let QR_img = document.getElementById("QR_img");
   let QR_text = document.getElementById("QR_text");
 
-  QR_img.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + QR_text.value;
+  QR_img.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
+    QR_text.value;
   imgBox.classList.add("show-img");
 
   QR_img.addEventListener("load", (e) => {
@@ -32,4 +34,31 @@ document.querySelector(".generate button").addEventListener("click", (e) => {
   });
 });
 
+import QrScanner from "https://cdnjs.cloudflare.com/ajax/libs/qr-scanner/1.4.1/qr-scanner.min.js";
 
+document
+  .querySelector('.scan .type input[type="file"]')
+  .addEventListener("change", (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files["0"]);
+    reader.addEventListener("load", (e) => {
+      document.querySelector(
+        ".scan .show"
+      ).innerHTML = `<img src=${e.target.result} />`;
+
+      QrScanner.scanImage(document.querySelector(".scan .show img"))
+        .then(
+          (result) =>{
+            document.querySelector(".scan .output").classList.remove("hidden");
+            document.querySelector(".scan .output").value = result;
+          } 
+        )
+        .catch(
+          (error) => {
+            document.querySelector(".scan .output").classList.remove("hidden");
+            document.querySelector(".scan .output").value = "No QR code found!";
+          }
+            
+        );
+    });
+  });
