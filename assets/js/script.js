1cc1ab88260bedb4e0adce4f7ed4b84e9211af1e
harikarthik-s <harikarthiks15@gmail.com>
2024-03-17 22:22:05 +0530

    QR generator added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,26 @@ generateBtn.addEventListener("click", () => {
   scan.hidden = true;
 });
 
-scanBtn.addEventListener('click', ()=>{
-    scanBtn.classList.add("active");
-    generateBtn.classList.remove("active");
-    scan.hidden= false;
-    generate.hidden = true;
-})
+scanBtn.addEventListener("click", () => {
+  scanBtn.classList.add("active");
+  generateBtn.classList.remove("active");
+  scan.hidden = false;
+  generate.hidden = true;
+});
+
+document.querySelector(".generate button").addEventListener("click", (e) => {
+  let imgBox = document.getElementById("imgBox");
+  let QR_img = document.getElementById("QR_img");
+  let QR_text = document.getElementById("QR_text");
+
+  QR_img.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + QR_text.value;
+  imgBox.classList.add("show-img");
+
+  QR_img.addEventListener("load", (e) => {
+    const download = document.querySelector(".generate a");
+    download.href = e.target.src;
+    download.classList.remove("hidden");
+  });
+});
+
+
